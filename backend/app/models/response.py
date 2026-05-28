f6a4a0fc83e9166d13890b828dc157e15523f4d2
Prajna1999 <gituprajna20@gmail.com>
2026-05-28 16:36:43 +0530

    test: breaking changes

--- backend/app/models/response.py
+++ backend/app/models/response.py
@@ -19,6 +19,7 @@ class ResponsesSyncAPIRequest(SQLModel):
     temperature: float = 0.1
     response_id: str | None = None
     question: str
+    lorem: str | None
 
     class Config:
         extra = "allow"
@@ -45,8 +46,8 @@ class FileResultChunk(SQLModel):
 
 
 class CallbackResponse(SQLModel):
-    status: str
-    response_id: str
+    # status: str
+    # response_id: str
     message: str
     diagnostics: Diagnostics | None = None
 
